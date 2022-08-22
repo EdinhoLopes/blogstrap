7d61f4c84b0c0e4d92260ae9a64b26cedf915700
Edson Lopes Gomes <edsonlopes@GVL-4A479SF6G.gabvice.ce.gov.br>
2022-08-22 14:05:15 -0300

    update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,9 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
   def new
     @article = Article.new
   end
@@ -17,6 +20,15 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render :new
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
     end
   end
 
