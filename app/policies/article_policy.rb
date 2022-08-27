c4c48cf2e951b3c5932f28d4b2246031d646afbd
Edinho Lopes <edsonlopesgomes7@gmail.com>
2022-08-26 21:36:39 -0300

    Autorização com Pundit(Continuação...)

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -9,13 +9,17 @@ class ArticlePolicy < ApplicationPolicy
     true
   end
 
+  def create?
+    user&.id
+  end
+
   #Se o id do usuário logado for igual ao id do usuário que criou o registro, retorna verdadeiro.
   def update?
-    user.id == record.user.id
+    user&.id == record.user.id
   end
 
   #Se o id do usuário logado for igual ao id do usuário que criou o registro, retorna verdadeiro.
   def destroy?
-    user.id == record.user.id
+    user&.id == record.user.id
   end
 end
