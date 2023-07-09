bb340bc77e298f4edacc83e5c60d1b6807959c8b
Edinho Lopes <edsonlopesgomes7@gmail.com>
2023-07-08 21:26:02 -0300

    show a article

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Rota raiz
   root 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/articles', to: 'articles#index'
+  #get '/articles', to: 'articles#index'
+  #get '/articles/:id', to: 'articles#show'
+
+  resources :articles
 end
